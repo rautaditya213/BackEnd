84d1b44a910a7f8d8454370e51eaa1c4b2d52804
aditya <rautaditya213@gmail.com>
2026-07-16 23:20:25 +0530

    layered the implementation

--- JAVA Full STACK/first/src/main/java/com/demo/first/app/UserController.java
+++ JAVA Full STACK/first/src/main/java/com/demo/first/app/UserController.java
@@ -4,116 +4,100 @@ package com.demo.first.app;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+
 
 @RestController
 @RequestMapping("/users")
 public class UserController {
 
-    // In-memory database to store users
-    private Map<Integer , User> userDb = new HashMap<>();
+    private UserService userService = new UserService();
 
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     // Create user
     @PostMapping
     public ResponseEntity<User> creatUser(@RequestBody User user){
-        userDb.putIfAbsent(user.getId(),user);
-        System.out.println("User created: " + user.getName() + ", Email: " + user.getEmail());
-        return ResponseEntity.status(HttpStatus.CREATED).body(user);
+       User createdUser = userService.createUser(user);
+        return ResponseEntity.status(HttpStatus.CREATED).body(createdUser);
     }
 
 
     // Update user
     @PutMapping
     public ResponseEntity<User> updateUser(@RequestBody User user){
-        if(userDb.containsKey(user.getId()))
-        {
-            userDb.put(user.getId(),user);
-            System.out.println("User updated: " + user.getName() + ", Email: " + user.getEmail());
-        }
-        else{
-            System.out.println("User with ID " + user.getId() + " not found.");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
-        return ResponseEntity.status(HttpStatus.OK).body(user);
+        User updated = userService.updateUser(user);
+        if(updated==null) return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(updated);
     }
 
 
     // Delete user
     @DeleteMapping
     public ResponseEntity<User> deleteUser(@RequestBody User user){
-        if(userDb.containsKey(user.getId())){
-            userDb.remove(user.getId());
-            System.out.println("User deleted: "+ user.getName() + ", Email: " + user.getEmail());
-            return ResponseEntity.status(HttpStatus.OK).body(user);
-        }
-        else{
-            System.out.println("User with id" + user.getId()+ "not found.");
-            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
-        }
+
+        boolean deleted = userService.deleteUser(user);
+        if(deleted) return ResponseEntity.status(HttpStatus.OK).body(user);
+        else return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
     }
 
 
-    // Delete user by id
-    @DeleteMapping("/{id}")
-    public String deleteUserById(@PathVariable int id){
-        if(userDb.containsKey(id)){
-            User user = userDb.get(id);
-            userDb.remove(id);
-            System.out.println("User deleted: "+ user.getName() + ", Email: " + user.getEmail());
-            return "User deleted successfully";
-        }
-        else{
-            System.out.println("User with id" + id + "not found.");
-            return "User with id" + id + "not found.";
-        }
-    }
-
+//    // Delete user by id
+//    @DeleteMapping("/{id}")
+//    public String deleteUserById(@PathVariable int id){
+//        if(userDb.containsKey(id)){
+//            User user = userDb.get(id);
+//            userDb.remove(id);
+//            System.out.println("User deleted: "+ user.getName() + ", Email: " + user.getEmail());
+//            return "User deleted successfully";
+//        }
+//        else{
+//            System.out.println("User with id" + id + "not found.");
+//            return "User with id" + id + "not found.";
+//        }
+//    }
 
     // Get all users
     @GetMapping
-    public ResponseEntity<Map<Integer, User> >getAllUsers(){
-        return ResponseEntity.status(HttpStatus.FOUND).body(userDb);
+    public List <User> getAllUsers(){
+        return userService.getAllUsers();
     }
 
     // Path variable example
     // Get user by id and orderId
-    @GetMapping("/{id}/orders/{orderId}")
-    public ResponseEntity<User> getUserOrder(@PathVariable(required = false)Integer id,@PathVariable(required = false) Integer orderId)
-    {
-        User user = userDb.get(id);
-        System.out.println("User with id " + id + " and orderId " + orderId + " found: " +
-                user.getName() + ", Email: " + user.getEmail());
-        return ResponseEntity.status(HttpStatus.OK).body(user);
-    }
+//    @GetMapping("/{id}/orders/{orderId}")
+//    public ResponseEntity<User> getUserOrder(@PathVariable(required = false)Integer id,@PathVariable(required = false) Integer orderId)
+//    {
+//        User user = userDb.get(id);
+//        System.out.println("User with id " + id + " and orderId " + orderId + " found: " +
+//                user.getName() + ", Email: " + user.getEmail());
+//        return ResponseEntity.status(HttpStatus.OK).body(user);
+//    }
 
     // Get user by id single User only
     // path variable name must be same as the parameter name in the method
     @GetMapping("/{id}")
     public ResponseEntity<User> getUser(@PathVariable(required = false) Integer id)
     {
-        User user = userDb.get(id);
+        User user = userService.getUserById(id);
+        if(user==null){
+            return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+        }
         return ResponseEntity.status(HttpStatus.OK).body(user);
     }
 
-    //@Getmapping("/users","/users/{id}") this can be possible but not recommended because it will create ambiguity in the request mapping and will throw an error.
-    //Request parameter example
+    // @Getmapping("/users","/users/{id}") this can be possible but not recommended because it will create ambiguity in the request mapping and will throw an error.
+    // Request parameter example
     // /search?name=john sample example of query parameter
     @GetMapping("/search")
-    public ResponseEntity<List<User>> searchUser(@RequestParam(required = false) Integer id,
-                                                 @RequestParam(required = false) String name,
-                                                 @RequestParam(required = false) String email)
+    public ResponseEntity<List<User>> searchUser( @RequestParam(required = false) Integer id,
+                                                  @RequestParam(required = false) String name,
+                                                  @RequestParam(required = false) String email )
     {
-        System.out.println(id);
-        System.out.println("Searching user with id " + id + ", name " + name + ", email " + email);
-        List<User> users =userDb.values().stream().filter
-                (u-> (id != null && u.getId() == id) ||
-                          (name != null && name.equalsIgnoreCase(u.getName())) ||
-                          (email != null && email.equalsIgnoreCase(u.getEmail()))).toList();
-        return ResponseEntity.status(HttpStatus.OK).body(users);
+       return ResponseEntity.status(HttpStatus.OK).body(userService.searchUsers(id,name,email));
     }
 
 
@@ -122,6 +106,5 @@ public class UserController {
     {
         System.out.println("User-Agent: " + userAgent);
         return "User-Agent: " + userAgent;
-
     }
 }
---
+++ JAVA Full STACK/first/src/main/java/com/demo/first/app/UserService.java
@@ -0,0 +1,71 @@
+package com.demo.first.app;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@Service
+public class UserService {
+    private Map<Integer , User> userDb = new HashMap<>();
+
+
+    public User createUser(User user){
+        System.out.println("User created: " + user.getName() + ", Email: " + user.getEmail());
+        userDb.putIfAbsent(user.getId(), user);
+        return user;
+    }
+
+
+    public User updateUser(User user) {
+        if(userDb.containsKey(user.getId()))
+        {
+            userDb.put(user.getId(),user);
+            System.out.println("User updated: " + user.getName() + ", Email: " + user.getEmail());
+        }
+        else{
+            System.out.println("User with ID " + user.getId() + " not found.");
+           return null;
+        }
+        return user;
+    }
+
+    public boolean deleteUser(User user) {
+        if(userDb.containsKey(user.getId())){
+            userDb.remove(user.getId());
+            System.out.println("User deleted: "+ user.getName() + ", Email: " + user.getEmail());
+            return true;
+        }
+        else{
+            System.out.println("User with id" + user.getId()+ "not found.");
+           return false;
+        }
+    }
+
+    public List<User> getAllUsers() {
+        return new ArrayList<>(userDb.values());
+    }
+
+    public User getUserById(Integer id) {
+        if (userDb.containsKey(id)){
+            return userDb.get(id);
+        }
+        else{
+            System.out.println("User with id" + id + "not found.");
+            return null;
+        }
+    }
+
+    public List<User> searchUsers(Integer id ,String name, String email) {
+
+               return userDb.values().stream().filter
+                        (u-> (id != null && u.getId() == id) ||
+                        (name != null && name.equalsIgnoreCase(u.getName())) ||
+                        (email != null && email.equalsIgnoreCase(u.getEmail()))).toList();
+
+    }
+}
